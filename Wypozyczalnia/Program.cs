556749a53f80eb6bc070af616a764b87cf0a7acd
Piotr <nume@spoko.pl>
2025-01-12 14:28:28 +0100

    etap 2

--- Wypozyczalnia/Program.cs
+++ Wypozyczalnia/Program.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using Wypozyczalnia.Models;
+using System.Collections.Generic;
 
 class Program
 {
+    static List<Movie> movies = new List<Movie>();
+    static int MovieIDc = 1;
+
+
     static void Main(string[] args)
     {
         while (true)
@@ -18,7 +24,7 @@ class Program
             switch (choice)
             {
                 case "1":
-                    Console.WriteLine("Movie manage selected");
+                    ManageMovies();
                     break;
                 case "2":
                     Console.WriteLine("Rental manage selected");
@@ -39,4 +45,140 @@ class Program
             }
         }
     }
+    // podmenu
+    static void ManageMovies()
+    {
+        while (true)
+        {
+            Console.WriteLine("\n--- Manage Movies ---");
+            Console.WriteLine("1. Add movie");
+            Console.WriteLine("2. View movies");
+            Console.WriteLine("3. Update movie");
+            Console.WriteLine("4. Delete movie");
+            Console.WriteLine("5. Main Menu");
+            Console.Write("Choose option: ");
+
+            var choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    MovieADD();
+                    break;
+                case "2":
+                    ViewMovies();
+                    break;
+                case "3":
+                    UpdateMovie();
+                    break;
+                case "4":
+                    DeleteMovie();
+                    break;
+                case "5":
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+        }
+    }
+    static void MovieADD()
+    {
+        Console.WriteLine("\nMovie title: ");
+        string title = Console.ReadLine();
+
+        Console.Write("Movie genre: ");
+        string genre = Console.ReadLine();
+
+        var NewMovie = new Movie
+        {
+            ID = MovieIDc++,
+            Title = title,
+            Genre = genre,
+            Available = true
+        };
+
+        movies.Add(NewMovie);
+        Console.WriteLine("Movie added");
+
+    }
+    //wyswietl (dozr)
+    static void ViewMovies()
+    {
+        Console.WriteLine("\n--- Movie List ---");
+        if (movies.Count == 0)
+        {
+            Console.WriteLine("No movies available");
+            return;
+        }
+
+        foreach(var movie in movies)
+        {
+            Console.WriteLine(movie);
+        }
+    }
+    //dodawanie
+    static void UpdateMovie()
+    {
+        Console.WriteLine("\nEnter ID: ");
+        if (!int.TryParse(Console.ReadLine(), out int ID))
+        {
+            Console.WriteLine("Invalid ID");
+            return; 
+        }
+
+        var movie = movies.Find(m => m.ID == ID);
+        if (movie == null)
+        {
+            Console.WriteLine("Movie not found");
+            return;
+        
+        }
+
+        Console.WriteLine("Enter new title: ");
+        string newTitle = Console.ReadLine();
+        if (!string.IsNullOrEmpty(newTitle))
+        {
+            movie.Title = newTitle;
+
+        }
+
+        Console.Write("Enter new genre: ");
+        string newGenre = Console.ReadLine();
+        if(!string.IsNullOrEmpty(newGenre))
+        {
+            movie.Genre = newGenre;
+        }
+
+        Console.WriteLine("Movie updated");
+
+    }
+
+    //usun
+    static void DeleteMovie()
+    {
+
+        Console.WriteLine("\nEnter ID: ");
+        if (!int.TryParse(Console.ReadLine(), out int ID))
+        {
+            Console.WriteLine("Invalid ID ");
+            return; 
+        }
+
+        var movie = movies.Find(m => m.ID == ID);
+        if (movie == null)
+        {
+            Console.WriteLine("Movie not found");
+            return;
+        }
+
+        movies.Remove(movie);
+        Console.WriteLine("Movie deleted ");
+        
+    }
+
+
+
+
 }
